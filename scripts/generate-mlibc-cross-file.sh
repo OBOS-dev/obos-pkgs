4c69c9c90e4141a8985ce1b8ccef7d6c1c437123
Omar Berrow <omarkberrow@gmail.com>
2025-08-30 14:26:15 -0400

    Fix problems with bootstrap-gcc

--- scripts/generate-mlibc-cross-file.sh
+++ scripts/generate-mlibc-cross-file.sh
@@ -3,8 +3,8 @@ target_triplet=$1
 compiler_prefix=$2
 
 echo "[binaries]
-c = '$compiler_prefix-$target_triplet-gcc'
-cpp = '$compiler_prefix-$target_triplet-g++'
+c = '$compiler_prefix$target_triplet-gcc'
+cpp = '$compiler_prefix$target_triplet-g++'
 ar = '$target_triplet-ar'
 windres = '$target_triplet-windres'
 strip = '$target_triplet-strip'
@@ -12,6 +12,14 @@ strip = '$target_triplet-strip'
 [host_machine]
 system = '${target_triplet##*-}'
 cpu_family = '${target_triplet%-*}'
-cpu = '${target_triplet%-*}'
-endian = 'little'
-" > ../cross-file.txt
+cpu = '${target_triplet%-*}'" > ../cross-file.txt
+
+target_architecture=${target_triplet%%-*}
+
+if [ $target_architecture = 'x86_64' ]
+then
+echo "endian = 'little'" >> ../cross-file.txt
+elif [ $target_architecture = 'm68k' ]
+then
+echo "endian = 'big'" >> ../cross-file.txt
+fi
