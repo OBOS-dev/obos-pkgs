5489071bcc84d0e31b9802e3326fa13e6262ad4f
Omar Berrow <omarkberrow@gmail.com>
2025-08-30 15:10:05 -0400

    Fix configure error for coreutils on m68k

---
+++ scripts/configure-coreutils.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+repo_directory=$1
+prefix=$2
+target_triplet=$3
+target_architecture=${target_triplet%%-*}
+
+if [ $target_architecture = 'x86_64' ]
+then
+    year2038status=enable
+elif [ $target_architecture = 'm68k' ]
+then
+    year2038status=disable
+fi
+
+"$repo_directory/coreutils-9.5/configure" "--prefix" "$prefix" "--host=$target_triplet" "CPPFLAGS=-DSLOW_BUT_NO_HACKS=1 -DUSE_STATVFS=1" "CFLAGS=-USE_STATVFS=1" "--$year2038status-year2038"
