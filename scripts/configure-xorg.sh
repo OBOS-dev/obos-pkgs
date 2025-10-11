7b0da53227190beb4dd3eaf768d607c7c4e4f839
Omar Berrow <omarkberrow@gmail.com>
2025-10-11 08:40:16 -0400

    Port xorg-server and its dependencies

---
+++ scripts/configure-xorg.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/bash
+$2/$4-$5/configure \
+    --host=$3 \
+    --disable-pciaccess \
+    --disable-ipv6 \
+    --disable-libdrm \
+    --disable-glx \
+    --disable-int10-module \
+    --disable-vgahw \
+    --disable-glamor \
+    --disable-dri3 \
+    --disable-dri2 \
+    --disable-dri \
+    --disable-xephyr \
+    --disable-xnest \
+    --disable-xwayland \
+    --disable-config-udev \
+    --disable-systemd-logind \
+    --prefix=$1 \
+    --without-systemd-daemon \
+    --disable-selective-werror \
+    --disable-strict-compilation \
+    ac_cv_func_epoll_create1=no
