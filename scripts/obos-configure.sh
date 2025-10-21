1ca5d6e4aa35664ef8e3905324b9a234d6c1632c
Omar Berrow <omarkberrow@gmail.com>
2025-10-21 09:45:12 -0400

    Fix bug with oboskrnl recipe

--- scripts/obos-configure.sh
+++ scripts/obos-configure.sh
@@ -12,5 +12,8 @@ cmake \
 -S$2 -GNinja
 if [ $target_triplet = 'x86_64-obos' ]
 then
-    chmod +x $2/dependencies/hyper/hyper_install-linux-x86_64
+    if [ `uname` != *MSYS2* ]
+    then
+        chmod +x $2/dependencies/hyper/hyper_install-linux-x86_64
+    fi
 fi
