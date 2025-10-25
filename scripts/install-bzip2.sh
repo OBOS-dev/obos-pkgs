b67d948b0619038ecabeae5c5b500fcb936401c1
Omar Berrow <omarkberrow@gmail.com>
2025-10-25 09:13:55 -0400

    Fix bzip2

--- scripts/install-bzip2.sh
+++ scripts/install-bzip2.sh
@@ -2,7 +2,7 @@
 
 dest_dir=$1
 
-make install DESTDIR=$dest_dir
+make install PREFIX=$dest_dir
 cp -av libbz2.so.* ${dest_dir}/lib
 ln -sv libbz2.so.1.0.8 ${dest_dir}/lib/libbz2.so
 cp -v bzip2-shared ${dest_dir}/bin/bzip2
