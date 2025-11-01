cbc823af5dcc50f8cc09e10c0248b537e75c06d9
Omar Berrow <omarkberrow@gmail.com>
2025-11-01 09:11:23 -0400

    Add /var/ directories to distro-files

--- scripts/install-distro-files.sh
+++ scripts/install-distro-files.sh
@@ -1,9 +1,25 @@
-#!/bin/bash
+#!/usr/bin/bash
+_mkdir()
+{
+    mkdir -p $1
+    touch $1/.keep
+}
 cp -r $1/distro-files/* $2/
-rm $2/bin
-rm $2/sbin
-rm $2/lib
+rm -f $2/bin
+rm -f $2/sbin
+rm -f $2/lib
+rm -f $2/var/run
 ln -s /usr/bin $2/bin
 ln -s /usr/sbin $2/sbin
 ln -s /usr/lib $2/lib
-mkdir -p $2/tmp
+_mkdir "$2/var"
+_mkdir "$2/var/lib"
+_mkdir "$2/var/log"
+_mkdir "$2/var/spool"
+_mkdir "$2/var/lock"
+_mkdir "$2/var/cache"
+_mkdir "$2/var/tmp"
+ln -s /run $2/var/run
+_mkdir "$2/run"
+_mkdir "$2/dev"
+_mkdir "$2/tmp"
