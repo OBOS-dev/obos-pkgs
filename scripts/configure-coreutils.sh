5eaa09d8339330008437f4a3fccc5eaf30b8091e
Omar Berrow <omarkberrow@gmail.com>
2025-11-08 09:41:20 -0500

    Downgrade coreutils

--- scripts/configure-coreutils.sh
+++ scripts/configure-coreutils.sh
@@ -12,4 +12,9 @@ then
     year2038status=disable
 fi
 
+old_pwd=$PWD
+cd "$repo_directory/coreutils-9.5"
+# "$repo_directory/coreutils-9.5/bootstrap" "--gnulib-srcdir=$repo_directory/gnulib" --no-git --force
+autoreconf -fi
+cd $old_pwd
 "$repo_directory/coreutils-9.5/configure" "--prefix" "$prefix" "--host=$target_triplet" "CPPFLAGS=-DSLOW_BUT_NO_HACKS=1 -DUSE_STATVFS=1" "CFLAGS=-USE_STATVFS=1" "--$year2038status-year2038"
